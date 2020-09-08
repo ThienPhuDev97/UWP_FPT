d7e8cf889ec905a8f28614eec769a399e3633391
Do Thien Phu <thienphu.dev97@gmail.com>
2020-09-08 09:07:03 +0700

    update bai tap

---
+++ Example01/Email.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1907A.model
+{
+    class Email
+    {
+        public string NameEmail { get; set; }
+        public string TieuDe { get; set; }
+        public string Content { get; set; }
+
+        public Email(string nameEmail, string tieude, string content)
+        {
+            this.NameEmail = nameEmail;
+            this.TieuDe = tieude;
+            this.Content = content;
+        }
+        public override string ToString()
+        {
+            return NameEmail + " || " + TieuDe + "  || " + Content;
+        }
+    }
+}
